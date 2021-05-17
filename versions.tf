2c48c3a9d0d5b1d70d5faceb587019b2ffa06246
Alvaro Denis <denisacostaq@gmail.com>
2021-05-17 22:50:28 +0300

    Define terraform and aws provider versions

---
+++ versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = "~> 0.15.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.40"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
