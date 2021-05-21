10a8d6820d75855f87ffa2a6ce73d6d0647c19f8
Alvaro Denis <denisacostaq@gmail.com>
2021-05-21 20:32:11 +0300

    Add elastic load balancer

---
+++ elb.tf
@@ -0,0 +1,57 @@
+resource "aws_lb" "netflix-http" {
+  name = "netflix-http"
+  load_balancer_type = "application"
+  internal = false
+  security_groups = [ aws_security_group.allow_global_http.id ]
+  subnets = [ aws_subnet.public-1a.id, aws_subnet.public-1b.id ]
+
+  tags = {
+    Name = "netflix_http_load_balancer"
+  }
+}
+
+resource "aws_lb_target_group" "netflix" {
+  name     = "netflix"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.netflix.id
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.netflix-http.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.netflix.arn
+  }
+}
+
+resource "aws_lb_listener_rule" "static" {
+  listener_arn = aws_lb_listener.front_end.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.netflix.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/"]
+    }
+  }
+}
+
+resource "aws_lb_target_group_attachment" "netflix_home" {
+  target_group_arn = aws_lb_target_group.netflix.arn
+  target_id        = aws_instance.home-node[0].id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "netflix_stream" {
+  target_group_arn = aws_lb_target_group.netflix.arn
+  target_id        = aws_instance.stream-node[0].id
+  port             = 80
+}
