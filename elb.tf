130e79b0fc308af844cc2f2ad6394d44b5fca5c7
Alvaro Denis <denisacostaq@gmail.com>
2021-05-22 12:02:35 +0300

    let the vpc subnets spread over all the availability zones and instances

--- elb.tf
+++ elb.tf
@@ -1,9 +1,13 @@
+# resource "aws_s3_bucket" "netflix-load-balancer-logs" {
+#   name = "netflix-load-balancer-logs"
+# }
+
 resource "aws_lb" "netflix-http" {
   name = "netflix-http"
   load_balancer_type = "application"
   internal = false
   security_groups = [ aws_security_group.allow_global_http.id ]
-  subnets = [ aws_subnet.public-1a.id, aws_subnet.public-1b.id ]
+  subnets = [for np in aws_subnet.netflix-public: np.id]
 
   tags = {
     Name = "netflix_http_load_balancer"
@@ -11,30 +15,37 @@ resource "aws_lb" "netflix-http" {
 }
 
 resource "aws_lb_target_group" "netflix" {
-  name     = "netflix"
+  for_each = toset(var.services)
+  name     = each.value
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.netflix.id
 }
 
-resource "aws_lb_listener" "front_end" {
+resource "aws_lb_listener" "netflix" {
   load_balancer_arn = aws_lb.netflix-http.arn
   port              = "80"
   protocol          = "HTTP"
-
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.netflix.arn
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Page not found"
+      status_code  = "404"
+    }
   }
 }
 
-resource "aws_lb_listener_rule" "static" {
-  listener_arn = aws_lb_listener.front_end.arn
-  priority     = 100
+resource "aws_lb_listener_rule" "home" {
+  listener_arn = aws_lb_listener.netflix.arn
+  priority     = 80
 
   action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.netflix.arn
+    type             = "redirect"
+    redirect {
+      path = "/${var.services[0]}"
+      status_code = "HTTP_301"
+    }
   }
 
   condition {
@@ -44,14 +55,29 @@ resource "aws_lb_listener_rule" "static" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "netflix_home" {
-  target_group_arn = aws_lb_target_group.netflix.arn
-  target_id        = aws_instance.home-node["eu-central-1c_home"].id
-  port             = 80
+resource "aws_lb_listener_rule" "service" {
+  for_each = toset(var.services)
+  listener_arn = aws_lb_listener.netflix.arn
+  priority     = index(var.services, each.value) + 1
+
+  action {
+    type             = "forward"
+    target_group_arn = lookup(aws_lb_target_group.netflix, each.value).arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/${each.value}*"]
+    }
+  }
 }
 
-resource "aws_lb_target_group_attachment" "netflix_stream" {
-  target_group_arn = aws_lb_target_group.netflix.arn
-  target_id        = aws_instance.stream-node["eu-central-1c_home"].id
+resource "aws_lb_target_group_attachment" "netflix" {
+  for_each = {
+    #make in a single place locals
+    for a in toset(local.instances-data) : format("%s_%s", a.az, a.service) => a
+  }
+  target_group_arn = lookup(aws_lb_target_group.netflix, each.value.service).arn
+  target_id        = aws_instance.worker-node[each.key].id
   port             = 80
 }
--- setup_worker_node.tpl
+++ setup_worker_node.tpl
@@ -3,4 +3,5 @@ sudo apt-get update
 sudo apt-get install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
-echo "<h1>${worker_name} running in ${az}</h1>" | sudo tee /var/www/html/index.html
+sudo mkdir -p /var/www/html/${worker_name}
+echo "<h1>${worker_name} running in ${az}</h1>" | sudo tee /var/www/html/${worker_name}/index.html
