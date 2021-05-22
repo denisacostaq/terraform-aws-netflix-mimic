d7f84a8ab2586816913c26f2568e3cde21cabfa3
Alvaro Denis <denisacostaq@gmail.com>
2021-05-22 11:54:15 +0300

    let the aim instances spread over all the availability zones

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "bastion-host" {
   ami = data.aws_ami.ubuntu-server.id
   instance_type = var.instance_type
-  subnet_id = aws_subnet.public-1a.id
+  subnet_id = lookup(aws_subnet.netflix-public, data.aws_availability_zones.all.names[0]).id
   security_groups = [ aws_security_group.allow_global_ssh.id ]
   key_name = "awsec2"
   tags = {
@@ -25,25 +25,14 @@ resource "aws_instance" "worker-node" {
   instance_type = var.instance_type
   key_name = "awsec2"
   for_each = {
+    #make in a single place
     for a in toset(local.instances-data): format("%s_%s", a.az, a.service) => a
   }
-  subnet_id = aws_subnet.private-1a.id
+  subnet_id = lookup(aws_subnet.netflix-private, each.value.az).id
   security_groups = [ aws_security_group.allow_public_ssh.id, aws_security_group.allow_public_http.id, aws_security_group.http_global_outgoing.id ]
-  user_data = templatefile("${path.module}/setup_apache.tpl", {worker_name = each.value.service, az = each.value.az})
+  user_data = templatefile("${path.module}/setup_worker_node.tpl", {worker_name = each.value.service, az = each.value.az})
   tags = {
-    "Name" = "Stream node"
+    "Name" = "Worker node ${each.value.service} in ${each.value.az}"
   }
 }
 
-resource "aws_instance" "home-node" {
-  ami = data.aws_ami.ubuntu-server.id
-  instance_type = var.instance_type
-  key_name = "awsec2"
-  count = 1
-  user_data = file("worker_node.sh")
-  subnet_id = aws_subnet.private-1b.id
-  security_groups = [ aws_security_group.allow_public_ssh.id, aws_security_group.allow_public_http.id, aws_security_group.http_global_outgoing.id ]
-  tags = {
-    "Name" = "Home node"
-  }
-}
